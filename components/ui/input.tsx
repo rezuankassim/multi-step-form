a996a8b24cecbae29d68bc73023a848646cf32c2
Rezuan Kassim <rezuankassim@hotmail.com>
2023-04-22 10:40:06 +0800

    added error state in input and label

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,21 +2,26 @@ import * as React from 'react';
 
 import {cn} from '@/lib/utils';
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  error?: boolean;
+}
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(({className, type, ...props}, ref) => {
-  return (
-    <input
-      type={type}
-      className={cn(
-        'flex h-10 w-full rounded border border-[#D6D9E6] bg-transparent px-4 pb-3 pt-[11px] font-medium leading-[18px] text-[#022959] ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-[#9699AA] focus-visible:border-[#483EFF] focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
-  );
-});
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({className, type, error = false, ...props}, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          'flex h-10 w-full rounded border bg-transparent px-4 pb-3 pt-[11px] font-medium leading-[18px] text-[#022959] ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-[#9699AA] focus-visible:border-[#483EFF] focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
+          error ? 'border-[#EE374A]' : 'border-[#D6D9E6]',
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
 Input.displayName = 'Input';
 
 export {Input};
--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -10,11 +10,23 @@ const labelVariants = cva(
   'text-xs leading-[14px] text-[#022959] peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
 );
 
+export interface LabelProps {
+  error?: string;
+}
+
 const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> & VariantProps<typeof labelVariants>
->(({className, ...props}, ref) => (
-  <LabelPrimitive.Root ref={ref} className={cn(labelVariants(), className)} {...props} />
+  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
+    VariantProps<typeof labelVariants> &
+    LabelProps
+>(({className, error, ...props}, ref) => (
+  <div className="flex items-center justify-between">
+    <LabelPrimitive.Root ref={ref} className={cn(labelVariants(), className)} {...props} />
+
+    {error ? (
+      <span className="text-xs font-bold leading-[14px] text-[#EE374A]">{error}</span>
+    ) : null}
+  </div>
 ));
 Label.displayName = LabelPrimitive.Root.displayName;
 
